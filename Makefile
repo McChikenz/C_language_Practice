4c34818fdd952a216e8a0f54e93e9bfc07d6f655
Jeo0 <jjrelb@gmail.com>
2024-10-15 20:01:49 +0800

    jojojo test

---
+++ Makefile
@@ -0,0 +1,12 @@
+default:
+	gcc ".\guessinggame_c_language-while_loop\main.c" 
+	./a.exe
+
+guess:
+	gcc ".\guessinggame_c_language-while_loop\main.c" 
+	./a.exe
+
+pointer:
+	gcc ".\pointer_dereference_-_c_language\main.c" 
+	./a.exe
+
--- guessinggame_c_language-while_loop/main.c
+++ guessinggame_c_language-while_loop/main.c
@@ -1,65 +1,50 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 
 int main()
 {
-    
-    /**
-     * Guessing game
-     * 
-     * */
-     
-     int userIn;
-     int secretNum = 98;
-     
-    //  printf("guess my number: ");
-    //  scanf("%d", &userIn);
-     
-    //  while(userIn != secretNum) { // to guess  infinitly
-         
-    //  printf("guess my number: ");
-    //  scanf("%d", &userIn);
 
-    //  }
-     
-    //   printf(" you guess my number %d", secretNum);
-     //gues a number with a limit;
-     int guestCount = 0;
-     int guessLimit = 3;
-     int outOfGuesses = 0;
-     
-     while(userIn != secretNum && outOfGuesses ==0) { // to guess  infinitly
-         if(guestCount < guessLimit ) {
-            printf("guess my number: ");
-            scanf("%d", &userIn);
-             guestCount++;
+	/**
+	 * Guessing game
+	 * 
+	 * */
 
-         } else {
-             outOfGuesses = 1;
-         }
-         
-         
+	int userIn = 0;
+	const int secretNum = 98;
 
+	//  printf("guess my number: ");
+	//  scanf("%d", &userIn);
 
-     }
-     
-     if(outOfGuesses == 1) {
-                   printf("out of gueses");
+	//  while(userIn != secretNum) { // to guess  infinitly
 
-     } else {
-         printf("You win");
-     }
+	//  printf("guess my number: ");
+	//  scanf("%d", &userIn);
+
+	//  }
+
+	//   printf(" you guess my number %d", secretNum);
+	//gues a number with a limit;
+	const int guessLimit = 3;  		// make constants YEEEEEE
+	int guestCount = 0;
+	// int outOfGuesses = 0; 		// dis problem may not need such flags
+
+	//while(userIn != secretNum && outOfGuesses ==0) { // to guess  infinitly
+	while(userIn != secretNum){
+		printf("guess my number: ");
+		scanf("%d", &userIn);
+		guestCount++;
+
+		if(guestCount == guessLimit) 	// once guestCount reached 3 times, byebye
+			break;
+	}
+
+	if(userIn == secretNum)
+		printf("You win\n");
+	else
+		printf("out of gueses\n");
 
 
-     
-    printf("Hello World");
 
-    return 0;
+	printf("Hello World\n");
+
+	return 0;
 }
